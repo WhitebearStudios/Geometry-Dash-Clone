80c4fd5e6278bed60806ac695451eb4d8c62e2bf
Maddox <maddoxriley314@gmail.com>
2026-09-08 18:59:39 -0500

    Added jumping

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,16 +4,26 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(SpriteRenderer))]
 public class PlayerManager : MonoBehaviour
 {
+    public float jumpForce = 5;
+
     public GameManager.Gamemode startGameMode = GameManager.Gamemode.Cube;
 
     [SerializeField] private Sprite cubeSprite, shipSprite;
     private SpriteRenderer spriteRenderer;
+    public GameManager.Gamemode CurrMode { get; private set; }
+
+    
+
+    [SerializeField] private LayerMask groundLayer, spikesLayer;
+    private Rigidbody2D rb;
+    private bool isGrounded;
 
     private InputAction jumpAction;
 
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
 
         jumpAction = InputSystem.actions.FindAction("Jump");
 
@@ -22,11 +32,31 @@ public class PlayerManager : MonoBehaviour
 
     private void Update()
     {
-        if (jumpAction.IsPressed()) print("Jump!");
+        if (jumpAction.IsPressed() && (CurrMode == GameManager.Gamemode.Ship || isGrounded))
+        {
+            //print("Jump!");
+
+            // Reset Y velocity for consistent jump height
+            rb.linearVelocityY = 0;
+
+            rb.AddForceY(jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if colliding with the ground
+        if ((groundLayer.value & (1 << collision.gameObject.layer)) != 0)
+        {
+            isGrounded = true;
+        }
     }
 
     public void SwitchGamemode(GameManager.Gamemode gamemode)
     {
+        CurrMode = gamemode;
+
         if(gamemode == GameManager.Gamemode.Cube)
         {
             spriteRenderer.sprite = cubeSprite;
